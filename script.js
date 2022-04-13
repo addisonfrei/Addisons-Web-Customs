0eb0057cd432a8376ab0915a7b88abc5ece2cc6a
Addison <addison.frei@icloud.com>
2022-04-13 17:49:10 -0400

    Updated formatting

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ document.querySelector("body").appendChild(h2);
 
 function changeColor() {
     el = document.getElementById("test")
-    if (el.style.color === 'black') {
-       el.style.color = 'white';
+    if (el.style.color === 'white') {
+       el.style.color = 'red';
     } else {
-        el.style.color = 'black';
+        el.style.color = 'white';
     }
 }
