1bdf151e78cb93eaf65920755a9ec2cfe118b581
Addison <addison.frei@icloud.com>
2022-04-13 17:28:08 -0400

    Updates to HTML, CSS, JS

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content was added by JavaScript";
+h2.textContent = "Showcasing My Pre-Work Development";
 document.querySelector("body").appendChild(h2);
+
+
+
+function changeColor() {
+    el = document.getElementById("test")
+    if (el.style.color === 'black') {
+       el.style.color = 'white';
+    } else {
+        el.style.color = 'black';
+    }
+}
