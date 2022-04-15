0073a90f1766fa8564577b7e4944f5bbd82d1bfa
Addison <addison.frei@icloud.com>
2022-04-15 18:52:36 -0400

    rewrite paragraph

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "Showcasing My Pre-Work Development";
 document.querySelector("body").appendChild(h2);
 
-
+const el = document.getElementById("test");
 
 function changeColor() {
-    el = document.getElementById("test")
     if (el.style.color === 'white') {
        el.style.color = 'red';
     } else {
